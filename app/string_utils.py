6ff4ed6d998b8e932d94a3e5c28c5c419f5a9a52
yeswanth chirumamilla <yeswanth.chirumamilla@celigo.com>
2026-04-15 10:16:44 +0530

    Add missing docstrings to functions in string_utils.py

--- app/string_utils.py
+++ app/string_utils.py
@@ -2,10 +2,12 @@
 
 
 def reverse_string(s):
+    """Return the reverse of the string s."""
     return s[::-1]
 
 
 def count_vowels(s):
+    """Return the number of vowels in string s."""
     return sum(1 for c in s if c in "aeiou")
 
 
@@ -15,10 +17,12 @@ def find_word(text, word):
 
 
 def capitalize_words(s):
+    """Return a string with each word in s capitalized."""
     return " ".join(w.capitalize() for w in s.split())
 
 
 def truncate(s, max_length):
+    """Truncate s to at most max_length characters."""
     if len(s) <= max_length:
         return s
     return s[:max_length]
